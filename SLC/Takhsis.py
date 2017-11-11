6a76e86256e3d0812b136c23fdc229af48cfd79e
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-11 12:23:02 +0100

    Now works with different number of main and subs players

--- SLC/Takhsis.py
+++ SLC/Takhsis.py
@@ -8,19 +8,22 @@ def Takhsis(league_main, league_subs, fitness_main, fitness_subs, settings):
     nsubs = settings['nsubs']
     dim = settings['dim']
 
-    league = np.append(league_main, league_subs, axis=0).reshape(nteams*(nmain+nsubs), dim)
+    league = np.append(league_main, league_subs).reshape(nteams*(nmain+nsubs),dim)
     fitness = np.append(fitness_main, fitness_subs)
 
     order = np.argsort(fitness)
     league = league[order]
     fitness = fitness[order]
 
-    league = np.reshape(league, (2*nteams, nmain, dim))
-    fitness = np.reshape(fitness, (2*nteams, nmain))
+    trues = [True for _ in range(nmain)]
+    falses = [False for _ in range(nsubs)]
+    per_team = np.append(trues, falses)
+    per_league = np.array([per_team for _ in range(nteams)]).flatten()
 
-    league_main = league[0:2*nteams:2]
-    league_subs = league[1:2*nteams:2]
-    fitness_main = fitness[0:2*nteams:2]
-    fitness_subs = fitness[1:2*nteams:2]
+    league_main = league[per_league].reshape(nteams,nmain,dim)
+    league_subs = league[~per_league].reshape(nteams,nsubs,dim)
+
+    fitness_main = fitness[per_league]
+    fitness_subs = fitness[~per_league]
 
     return league_main, league_subs, fitness_main, fitness_subs
