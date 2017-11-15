993f735eb661dc5798090fc167131bd4b9067de6
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-15 21:05:00 +0100

    Back to calculating costs with inverse of mean fitness

--- SLC/ProbabilityHost.py
+++ SLC/ProbabilityHost.py
@@ -3,11 +3,13 @@ import random
 
 def ProbabilityHost(i, j, league_main, fitness_main):
 
-    icost = np.mean(fitness_main[i])
-    jcost = np.mean(fitness_main[j])
+    icost = 1/np.mean(fitness_main[i])
+    jcost = 1/np.mean(fitness_main[j])
 
     phost = icost/(icost+jcost)
     rand = random.uniform(0,1)
+
+    print ("phost:", phost, "icost:", icost, "jcost:", jcost, "rand:", rand)
 
     if rand<phost:
         winner = i
