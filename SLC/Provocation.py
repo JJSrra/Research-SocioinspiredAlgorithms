db03ba369274aea18b4af80c52ec23fe0aa154b4
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-16 18:33:55 +0100

    Provocation affects only weakest substitute player

--- SLC/Provocation.py
+++ SLC/Provocation.py
@@ -9,29 +9,28 @@ def Provocation(winner, league_main, league_subs, fitness_subs, settings):
     upper_bound = settings['upper_bound']
     dim = settings['dim']
 
-    for i in range (0, nsubs):
-        gravity = np.mean(league_main[winner], axis=0)
-        chi_1 = random.uniform(0.9, 1)
-        chi_2 = random.uniform(0.4, 0.6)
+    gravity = np.mean(league_main[winner], axis=0)
+    chi_1 = random.uniform(0.9, 1)
+    chi_2 = random.uniform(0.4, 0.6)
 
-        new_player = np.clip(gravity + chi_1*(gravity - league_subs[winner][i]), lower_bound, upper_bound)
+    new_player = np.clip(gravity + chi_1*(gravity - league_subs[winner][nsubs-1]), lower_bound, upper_bound)
+    new_fitness = TestFoo(new_player)
+    settings['neval'] += 1
+
+    if new_fitness < fitness_subs[winner][nsubs-1]:
+        league_subs[winner][nsubs-1] = new_player
+        fitness_subs[winner][nsubs-1] = new_fitness
+    else:
+        new_player = np.clip(gravity + chi_2*(league_subs[winner][nsubs-1] - gravity), lower_bound, upper_bound)
         new_fitness = TestFoo(new_player)
         settings['neval'] += 1
 
-        if new_fitness < fitness_subs[winner][i]:
-            league_subs[winner][i] = new_player
-            fitness_subs[winner][i] = new_fitness
+        if new_fitness < fitness_subs[winner][nsubs-1]:
+            league_subs[winner][nsubs-1] = new_player
+            fitness_subs[winner][nsubs-1] = new_fitness
         else:
-            new_player = np.clip(gravity + chi_2*(league_subs[winner][i] - gravity), lower_bound, upper_bound)
-            new_fitness = TestFoo(new_player)
+            league_subs[winner][nsubs-1] = np.random.uniform(lower_bound, upper_bound, dim)
+            fitness_subs[winner][nsubs-1] = TestFoo(league_subs[winner][nsubs-1])
             settings['neval'] += 1
-
-            if new_fitness < fitness_subs[winner][i]:
-                league_subs[winner][i] = new_player
-                fitness_subs[winner][i] = new_fitness
-            else:
-                league_subs[winner][i] = np.random.uniform(lower_bound, upper_bound, dim)
-                fitness_subs[winner][i] = TestFoo(league_subs[winner][i])
-                settings['neval'] += 1
 
     return league_subs, fitness_subs, settings
