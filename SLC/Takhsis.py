edf6967bddbb2c6de81565bd1c5427c9aa8729fd
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-16 18:33:06 +0100

    Added comments

--- SLC/Takhsis.py
+++ SLC/Takhsis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from TestFoo import *
 
+# Reorders the league and assigns best players to first team (main and subs) and so on.
 def Takhsis(league_main, league_subs, fitness_main, fitness_subs, settings):
 
     nteams = settings['nteams']
@@ -8,13 +9,17 @@ def Takhsis(league_main, league_subs, fitness_main, fitness_subs, settings):
     nsubs = settings['nsubs']
     dim = settings['dim']
 
+    # Both league arrays are merged into a common one, as well as both fitness arrays
     league = np.append(league_main, league_subs).reshape(nteams*(nmain+nsubs),dim)
     fitness = np.append(fitness_main, fitness_subs)
 
+    # With the descending order of fitness given by argsort, both league and fitness arrays are reordered
     order = np.argsort(fitness)
     league = league[order]
     fitness = fitness[order]
 
+    # An array of trues and falses is created for selecting which players are main and which subs players,
+    # and to assign them to their matching team
     trues = [True for _ in range(nmain)]
     falses = [False for _ in range(nsubs)]
     per_team = np.append(trues, falses)
@@ -23,6 +28,7 @@ def Takhsis(league_main, league_subs, fitness_main, fitness_subs, settings):
     league_main = league[per_league].reshape(nteams,nmain,dim)
     league_subs = league[~per_league].reshape(nteams,nsubs,dim)
 
+    # This works as well for selecting which fitness values correspond to main and subs teams
     fitness_main = fitness[per_league].reshape(nteams,nmain)
     fitness_subs = fitness[~per_league].reshape(nteams,nmain)
 
