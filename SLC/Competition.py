5674e2ef18e5e384cfdc41d560b07439a376ce69
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-17 09:07:40 +0100

    Indexation typo

--- SLC/Competition.py
+++ SLC/Competition.py
@@ -13,4 +13,4 @@ def Competition(league_main,league_subs,fitness_main,fitness_subs,settings):
             league_main,fitness_main,settings = Imitation(winner,league_main,fitness_main,settings)
             league_subs,fitness_subs,settings = Provocation(winner,league_main,league_subs,fitness_subs,settings)
 
-return league_main,league_subs,fitness_main,fitness_subs,settings
+    return league_main,league_subs,fitness_main,fitness_subs,settings
