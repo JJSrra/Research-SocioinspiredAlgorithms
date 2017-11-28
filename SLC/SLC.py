39c9087dadbec485d742f134092fd0fd61bbd32e
Juanjo Sierra <jjsierra103@gmail.com>
2017-11-28 18:28:55 +0100

    Global algorithm function, parameters by default added in function definition

---
+++ SLC/SLC.py
@@ -0,0 +1,34 @@
+# Soccer League Competition Algorithm (For Continuous Decision Variables)
+# Developed By: Naser Moosavian
+# Translated to Python By: Juanjo Sierra
+
+import numpy as np
+from CreateInitialLeague import *
+from Competition import *
+from Takhsis import *
+
+def SLC(CostFunction, max_eval=50000, dim=30, nteams=5, nmain=10, nsubs=10, lower_bound=0,
+        upper_bound=10, max_it=10**8, mutation_probability=0.1, mutation_rate=0.2):
+
+    # Start number of evaluations
+    neval = 0
+
+    # Domain of the problem, tuple including lower and upper bounds
+    domain = (lower_bound, upper_bound)
+
+    # Creation of the initial league
+    league_main,league_subs,fitness_main,fitness_subs = CreateInitialLeague(CostFunction,
+                nteams, nmain, nsubs, dim, domain)
+
+    # Seasons keep on launching until 'max_it' seasons have been played, or until 'neval' reaches number of Cost
+    # Function evaluations that was indicated as a ceiling parameter
+    for it in range(0,max_it):
+        league_main,league_subs,fitness_main,fitness_subs,evals_competition = Competition(CostFunction,
+                    league_main,league_subs,fitness_main,fitness_subs,domain)
+        neval += evals_competition
+        league_main,league_subs,fitness_main,fitness_subs = Takhsis(league_main,league_subs,fitness_main,fitness_subs)
+
+        print("Season {:4}, best solution: {:e}".format(it, fitness_main[0][0]))
+
+        if (neval > max_eval) or (fitness_main[0][0] == 0.0):
+            break
---
+++ SLC/SLCmain.py
@@ -0,0 +1,5 @@
+from TestFoo import *
+from SLC import *
+
+if __name__ == "__main__":
+    SLC(TestFoo)
