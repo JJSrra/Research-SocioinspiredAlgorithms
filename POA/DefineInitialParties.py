af0322316c4672718feb3a3e8ac73660d92bc3c1
Juanjo Sierra <jjsierra103@gmail.com>
2018-03-11 13:21:34 +0100

    Members saved in list instead of arrays

--- POA/DefineInitialParties.py
+++ POA/DefineInitialParties.py
@@ -7,7 +7,8 @@ def DefineInitialParties(CostFunction, nparties, nmembers, ncandidates, dim, dom
 
     # First we create the regular members of each party; it has to be saved in a list
     # because the size of the parties may vary in following steps of the algorithm
-    members = np.random.uniform(lower_bound, upper_bound, nparties*nregularmembers*dim).reshape(nparties, nregularmembers, dim)
+    members = np.random.uniform(lower_bound, upper_bound, nparties*nregularmembers*dim).reshape(nparties*nregularmembers, dim)
+    members = np.split(members, nparties)
     members_fitness = np.apply_along_axis(CostFunction, 2, members)
 
     # And now we generate N candidate spots for each party, and assign each party's best members to them
