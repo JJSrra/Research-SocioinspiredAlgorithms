8a5f7b43a81577c314a48c549b801b1c3e1784d2
Juanjo Sierra <jjsierra103@gmail.com>
2018-03-11 13:33:45 +0100

    Members fitness also saved in a list

--- POA/DefineInitialParties.py
+++ POA/DefineInitialParties.py
@@ -8,8 +8,9 @@ def DefineInitialParties(CostFunction, nparties, nmembers, ncandidates, dim, dom
     # First we create the regular members of each party; it has to be saved in a list
     # because the size of the parties may vary in following steps of the algorithm
     members = np.random.uniform(lower_bound, upper_bound, nparties*nregularmembers*dim).reshape(nparties*nregularmembers, dim)
+    members_fitness = np.apply_along_axis(CostFunction, 1, members)
     members = np.split(members, nparties)
-    members_fitness = np.apply_along_axis(CostFunction, 2, members)
+    members_fitness = np.split(members_fitness, nparties)
 
     # And now we generate N candidate spots for each party, and assign each party's best members to them
     candidates = np.random.uniform(lower_bound, upper_bound, nparties*ncandidates*dim).reshape(nparties, ncandidates, dim)
