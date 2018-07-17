56c58394dd28e9773c7e841ed8ca201831872bf7
Juanjo Sierra <jjsierra103@gmail.com>
2018-07-17 19:22:04 +0200

    Created first population and fitness

---
+++ SEA/CreateInitialPopulation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def CreateInitialPopulation(CostFunction, nindividuals, dim, domain):
+    lower_bound, upper_bound = domain
+
+    # Create the N individuals with random positions in the search space
+    population = np.random.uniform(lower_bound, upper_bound, nindividuals*dim).reshape(nindividuals, dim)
+    population_fitness = np.apply_along_axis(CostFunction, 1, population)
+
+    print(population)
+    print("###############################################")
+    print(population_fitness)
--- SEA/SEA.py
+++ SEA/SEA.py
@@ -2,8 +2,13 @@
 # As seen in the paper by Yuechun Xu, Zhihua Cui & Jianchao Zeng
 # Implemented by Juanjo Sierra
 
+from CreateInitialPopulation import *
+
 def SEA(CostFunction, dim=10, nindividuals=20, max_iter=500,
     lower_bound=0, upper_bound=10):
 
     # Domain of the function, tuple including lower and upper bounds
     domain = (lower_bound, upper_bound)
+
+    # Create the first individuals
+    population, population_fitness = CreateInitialPopulation(CostFunction, nindividuals, dim, domain)
