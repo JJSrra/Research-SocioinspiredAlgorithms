01408ce2ae2ff2ed1061ed37e0c49402ed47241a
Juanjo Sierra <jjsierra103@gmail.com>
2018-07-22 20:17:34 +0200

    Added behaviour change for average population

--- SEA/BehaviourChanges.py
+++ SEA/BehaviourChanges.py
@@ -1,14 +1,25 @@
 import numpy as np
-from Manners import Manner2
+from Manners import Manner2, Manner3
 
 def BehaviourChanges(population, population_fitness, history, history_fitness, emotion, lower_threshold, upper_threshold, k1, k2, k3, domain):
     lower_bound, upper_bound = domain
     status_best = history[np.argsort(history_fitness)[0]]
 
+    emotion = np.random.uniform(0.2, 0.8, 20)
+
     # Select as 'bad population' individuals with emotion index lesser than lower threshold
     bad_population = np.where(emotion < lower_threshold)
 
-    if (len(bad_population[0] > 0)):
+    # Select as 'average population' individuals with emotion index between lower and upper thresholds
+    average_population = np.where(np.logical_and(emotion >= lower_threshold, emotion < upper_threshold))
+
+    # Perform appropriate change to bad population
+    if(len(bad_population[0] > 0)):
         population[bad_population] += np.array([Manner2(individual, status_best, k2) 
                 for individual in population[bad_population]])
+
+    # Perform appropriate change to average population
+    if(len(average_population[0] > 0)):
+        population[average_population] += np.array([Manner3(individual, individual_best, status_best, population, k1, k2, k3) 
+                for (individual, individual_best) in zip(population[average_population], history[average_population])])
     
