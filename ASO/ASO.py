0c0a0d95310bf4dbdfa96bdc94a6b2b883cac987
Juanjo Sierra <jjsierra103@gmail.com>
2018-08-02 20:06:29 +0200

    Calculated fickleness indexes

--- ASO/ASO.py
+++ ASO/ASO.py
@@ -3,9 +3,11 @@
 # Implemented by Juanjo Sierra
 
 from CreateInitialSociety import *
+from CurrentMovementPolicy import CalculateFicklenessIndexes
 
 def ASO(CostFunction, dim=10, nindividuals=20, max_iter=1000,
-    external_threshold=0.5, internal_threshold=0.5, lower_bound=0, upper_bound=10):
+    fickleness_rate=0.3, external_threshold=0.5, internal_threshold=0.5,
+	lower_bound=0, upper_bound=10):
 
     # Domain of the function, tuple including lower and upper bounds
 	domain = (lower_bound, upper_bound)
@@ -13,3 +15,10 @@ def ASO(CostFunction, dim=10, nindividuals=20, max_iter=1000,
     # Create the initial society
 	society, society_fitness, history, history_fitness = CreateInitialSociety(CostFunction,
             nindividuals, dim, domain)
+
+	# Start the main loop
+	iteration = 0
+	while iteration < max_iter:
+		fickleness_indexes = CalculateFicklenessIndexes(society_fitness, history_fitness, fickleness_rate)
+		
+		iteration += 1
---
+++ ASO/CurrentMovementPolicy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def Fickleness(current_fitness, best_fitness, best_past_fitness, fickleness_rate):
+    return 1 - fickleness_rate*(best_fitness/current_fitness) - (1-fickleness_rate)*(best_past_fitness/current_fitness)
+
+def CalculateFicklenessIndexes(society_fitness, history_fitness, fickleness_rate):
+    best = np.min(society_fitness)
+    
+    return np.array([Fickleness(current, best, past, fickleness_rate)
+        for (current, past) in zip(society_fitness, history_fitness)])
