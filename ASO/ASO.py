c89a2fc425c21fdd694d266f55c6c4020485af0b
Juanjo Sierra <jjsierra103@gmail.com>
2018-08-04 11:51:02 +0200

    Created new positions according to Current Movement Policy

--- ASO/ASO.py
+++ ASO/ASO.py
@@ -3,13 +3,14 @@
 # Implemented by Juanjo Sierra
 
 from CreateInitialSociety import *
-from CurrentMovementPolicy import CalculateFicklenessIndexes
+from CurrentMovementPolicy import CalculateFicklenessIndexes, GenerateCurrentMovementPolicy
 from SocietyMovementPolicy import CalculateExternalIrregularityIndexes
 from PreviousMovementPolicy import CalculateInternalIrregularityIndexes
 
-def ASO(CostFunction, dim=10, nindividuals=20, max_iter=1000,
+def ASO(CostFunction, dim=10, nindividuals=3, max_iter=1000,
     fickleness_rate=0.5, external_rate=4, external_threshold=0.5,
-	internal_rate=4, internal_threshold=0.5, lower_bound=0, upper_bound=10):
+	internal_rate=4, internal_threshold=0.5, evolution_rate=0.5,
+	lower_bound=0, upper_bound=10):
 
     # Domain of the function, tuple including lower and upper bounds
 	domain = (lower_bound, upper_bound)
@@ -22,9 +23,14 @@ def ASO(CostFunction, dim=10, nindividuals=20, max_iter=1000,
 	iteration = 0
 	while iteration < max_iter:
 		global_best = np.min(history_fitness)
+		iteration_best = society[np.argsort(society_fitness)[0]]
 
 		fickleness_indexes = CalculateFicklenessIndexes(society_fitness, history_fitness, fickleness_rate)
 		external_indexes = CalculateExternalIrregularityIndexes(society_fitness, global_best, external_rate)
 		internal_indexes = CalculateInternalIrregularityIndexes(society_fitness, history_fitness, internal_rate)
+
+		current_movement_positions = np.array([GenerateCurrentMovementPolicy(
+			individual, fickleness_index, iteration_best, fickleness_rate, evolution_rate, domain)
+			for (individual, fickleness_index) in zip(society, fickleness_indexes)])
 
 		iteration += 1
--- ASO/CurrentMovementPolicy.py
+++ ASO/CurrentMovementPolicy.py
@@ -8,3 +8,14 @@ def CalculateFicklenessIndexes(society_fitness, history_fitness, fickleness_rate
     
     return np.array([Fickleness(current, best, past, fickleness_rate)
         for (current, past) in zip(society_fitness, history_fitness)])
+
+def GenerateCurrentMovementPolicy(individual, fickleness_index, iteration_best, fickleness_rate, evolution_rate, domain):
+    lower_bound, upper_bound = domain
+    dim = len(individual)
+
+    if fickleness_index < fickleness_rate: 
+        mutation_multiplier = np.random.uniform(-1,1,10)
+        mutation = evolution_rate * mutation_multiplier * (iteration_best - individual)
+        return np.clip(individual + mutation, lower_bound, upper_bound)
+    else:
+        return np.random.uniform(lower_bound, upper_bound, dim)
