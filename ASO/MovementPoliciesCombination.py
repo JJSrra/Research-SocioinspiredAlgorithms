88c7479038bbfca03b50619b175db85767cf6e57
Juanjo Sierra <jjsierra103@gmail.com>
2018-08-05 20:36:56 +0200

    Returned fitness in different way for array consistency

--- ASO/MovementPoliciesCombination.py
+++ ASO/MovementPoliciesCombination.py
@@ -6,13 +6,13 @@ def SelectBestPolicy(CostFunction, current_policy, society_policy, previous_poli
     policies_fitness = np.apply_along_axis(CostFunction, 1, policies)
     
     best = np.argsort(policies_fitness)[0]
-    return policies[best], policies_fitness[best]
+    return np.append(policies_fitness[best], policies[best])
 
 def NewPositionsPolicyBased(CostFunction, current_policies, society_policies, previous_policies):
     results = np.array([SelectBestPolicy(CostFunction, current, society, previous)
 			for (current, society, previous) in zip(current_policies, society_policies, previous_policies)])
-    
-    new_positions = results[:, 0]
-    new_fitness = results[:, 1]
+
+    new_fitness = results[:, 0]
+    new_positions = np.delete(results, 0, 1)
 
     return new_positions, new_fitness
