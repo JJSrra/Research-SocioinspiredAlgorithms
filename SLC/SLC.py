0822180ca5875e1f30975d8f980a91c03e23129f
Juanjo Sierra <jjsierra103@gmail.com>
2018-09-02 11:12:51 +0200

    Added SLC convergence graph values

--- SLC/SLC.py
+++ SLC/SLC.py
@@ -8,30 +8,37 @@ from Competition import *
 from Takhsis import *
 
 def SLC(CostFunction, max_eval=50000, dim=30, nteams=5, nmain=10, nsubs=10, lower_bound=0,
-        upper_bound=10, max_it=10**8, mutation_probability=0.1, mutation_rate=0.2):
+		upper_bound=10, max_it=10**8, mutation_probability=0.1, mutation_rate=0.2):
 
-    # Domain of the problem, tuple including lower and upper bounds
-    domain = (lower_bound, upper_bound)
+	# Domain of the problem, tuple including lower and upper bounds
+	domain = (lower_bound, upper_bound)
 
-    # Creation of the initial league
-    league_main,league_subs,fitness_main,fitness_subs,initial_evals = CreateInitialLeague(CostFunction,
-                nteams, nmain, nsubs, dim, domain)
+	# Creation of the initial league
+	league_main,league_subs,fitness_main,fitness_subs,initial_evals = CreateInitialLeague(CostFunction,
+				nteams, nmain, nsubs, dim, domain)
 
-    # Starting number of evaluations
-    neval = initial_evals
+	# Starting number of evaluations
+	neval = initial_evals
 
-    # Seasons keep on launching until 'max_it' seasons have been played, or until 'neval' reaches number of Cost
-    # Function evaluations that was indicated as a ceiling parameter
-    for it in range(0,max_it):
-        league_main,league_subs,fitness_main,fitness_subs,evals_competition = Competition(CostFunction,
-                    league_main,league_subs,fitness_main,fitness_subs,domain,mutation_rate,mutation_probability)
-        neval += evals_competition
-        league_main,league_subs,fitness_main,fitness_subs = Takhsis(league_main,league_subs,fitness_main,fitness_subs)
+	evaluations_marker = 0
+	evaluation_marks = np.array([])
 
-        # print("Season {:4}, best solution: {:e}".format(it, fitness_main[0][0]))
-        # print("\tNumber of evaluations: {}".format(neval))
+	# Seasons keep on launching until 'max_it' seasons have been played, or until 'neval' reaches number of Cost
+	# Function evaluations that was indicated as a ceiling parameter
+	for it in range(0,max_it):
+		league_main,league_subs,fitness_main,fitness_subs,evals_competition = Competition(CostFunction,
+					league_main,league_subs,fitness_main,fitness_subs,domain,mutation_rate,mutation_probability)
+		neval += evals_competition
+		league_main,league_subs,fitness_main,fitness_subs = Takhsis(league_main,league_subs,fitness_main,fitness_subs)
 
-        if neval > max_eval:
-            break
+		# print("Season {:4}, best solution: {:e}".format(it, fitness_main[0][0]))
+		# print("\tNumber of evaluations: {}".format(neval))
 
-    return fitness_main[0][0]
+		if neval >= evaluations_marker:
+			evaluation_marks = np.insert(evaluation_marks, len(evaluation_marks), fitness_main[0][0])
+			evaluations_marker += max_eval / 10
+
+		if neval > max_eval:
+			break
+
+	return np.append(evaluation_marks, fitness_main[0][0])
